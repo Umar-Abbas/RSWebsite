f8be84f5d1e39521ece8b75c4c666a637ae950a8
Umar Abbas <omer.ithj@gmail.com>
2024-12-29 03:54:43 +0400

    ff

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -16,8 +16,7 @@ const Footer = () => {
           <div>
             <h2 className="text-2xl font-bold mb-4 text-green-500">ReactiveSpace</h2>
             <p className="text-gray-400">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
-              aliquam massa arcu, eget fringilla felis commodo at.
+            Building the future of AI applications.
             </p>
             <div className="flex mt-4">
               <a
